73dabcf5f69aa065ab1a4556ca7443dd8a47d198
Brandon Palm <bpalm@redhat.com>
2026-07-15 14:36:09 -0500

    feat: add kubeconfig-path output
    Expose the CRC kubeconfig file path as an action output so users
    can set KUBECONFIG for helm, kustomize, and other tools.
    
    Closes #76

--- scripts/get-cluster-credentials.sh
+++ scripts/get-cluster-credentials.sh
@@ -9,6 +9,9 @@ echo "::add-mask::$KUBEADMIN_PASSWORD"
 API_URL="https://api.crc.testing:6443"
 CONSOLE_URL="https://console-openshift-console.apps-crc.testing"
 
+KUBECONFIG_PATH="$HOME/.crc/machines/crc/kubeconfig"
+
 echo "api-url=$API_URL" >>"${GITHUB_OUTPUT}"
 echo "console-url=$CONSOLE_URL" >>"${GITHUB_OUTPUT}"
 echo "kubeadmin-password=$KUBEADMIN_PASSWORD" >>"${GITHUB_OUTPUT}"
+echo "kubeconfig-path=$KUBECONFIG_PATH" >>"${GITHUB_OUTPUT}"
